c01f6f7ba0faebc0a39f56f5a0468c6448462ca6
Arif Rahman <arifrhmn2304@gmail.com>
2023-07-28 17:50:10 +0700

    Pertama Ngoding

---
+++ test.js
@@ -0,0 +1,16 @@
+let Angka1 = 5;
+let Angka2 = 9; 
+console.log(Angka1 + Angka2, "pertambahan");
+console.log(Angka1 - Angka2, "pengurangan");
+console.log(Angka1 / Angka2, "pembagian");
+console.log(Angka1 * Angka2, "perkalian") 
+
+let firstname = 'Arif'
+let lastname = 'Rahman'
+let fullname = firstname + ` ` +lastname
+console.log(fullname);
+
+let number = 10;
+console.log(number + 10); //20
+number = 5;
+console.log(number - 3); //2
